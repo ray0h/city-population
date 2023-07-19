bdd24b98d863029f4fcd546605d4942e4cb7dc82
Ray Oh <raymond.oh@gmail.com>
2023-07-19 10:25:19 -0400

    set up caching for GET routes

--- routes/populationRoutes.js
+++ routes/populationRoutes.js
@@ -1,17 +1,32 @@
+import NodeCache from 'node-cache'
+
 export default function populationRoutes (app, options, done) {
   const populations = app.mongo.client.db("city-population").collection("populations")
+  const cache = new NodeCache({ stdTTL: 60 })
 
   const getPopulation = (req, reply) => {
     const city = req.params.city
     const state = req.params.state
-    const query = {"city": {"$regex": city, "$options": "i"}, "state": {"$regex": state, "$options": "i"}}
-    populations.findOne(query).then(value => {
-      if (value) {
-        reply.status(200)
-        reply.send({population: value.population})
+    const cacheKey = `${city}_${state}`
+
+    const cachedResponse = cache.get(cacheKey)
+    if (cachedResponse !== undefined) {
+      if (cachedResponse != "error") {
+        reply.status(200).send({ population: cachedResponse })
+        return
       } else {
-        reply.status(400)
-        reply.send({error: "The city and/or state could not be found."})
+        reply.status(400).send({error: "The city and/or state could not be found."})
+        return
+      }
+    }
+    const query = {"city": {"$regex": city, "$options": "i"}, "state": {"$regex": state, "$options": "i"}}
+    populations.findOne(query, { population: 1 }).then(value => {
+      if (value) {
+        cache.set(cacheKey, value.population)
+        reply.status(200).send({population: value.population})
+      } else {
+        cache.set(cacheKey, "error")
+        reply.status(400).send({error: "The city and/or state could not be found."})
       }
     })
   }
